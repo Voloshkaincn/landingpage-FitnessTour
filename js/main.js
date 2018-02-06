1f44d0ac32b067127977bc143e05a2c54d55fffe
voloshkaincn <barvinok.cn@gmail.com>
2018-02-07 01:18:18 +0200

    add aections

--- js/main.js
+++ js/main.js
@@ -2,5 +2,14 @@ jQuery(document).ready(function($) {
 
 	$("#fullpage").fullpage({
 	});
+  	$('.post-slider').slick({
+	    slidesToShow: 2,
+	    dots: true
+	});
+  	$('.views__slider').slick({
+	    slidesToShow: 3,
+	    dots: true
+	});
+
 
 })
