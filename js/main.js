107dd292d6b81e36fa6742bbe98ac51da081edf5
Voloshkaincn <barvinok.cn@gmail.com>
2018-02-07 12:00:38 +0200

    finished layout without mobile version

--- js/main.js
+++ js/main.js
@@ -2,13 +2,29 @@ jQuery(document).ready(function($) {
 
 	$("#fullpage").fullpage({
 	});
+
   	$('.post-slider').slick({
 	    slidesToShow: 2,
-	    dots: true
+	    dots: true,
+	    arrows: false
 	});
+	$('.post-slider__header .slider__prev').on('click', function(){
+		$('.post-slider').slick('slickPrev');
+	});
+	$('.post-slider__header .slider__next').on('click', function(){
+		$('.post-slider').slick('slickNext');
+	});
+
   	$('.views__slider').slick({
 	    slidesToShow: 3,
-	    dots: true
+	    dots: true,
+	    arrows: false
+	});
+	$('.views-slider__header .slider__prev').on('click', function(){
+		$('.views__slider').slick('slickPrev');
+	});
+	$('.views-slider__header .slider__next').on('click', function(){
+		$('.views__slider').slick('slickNext');
 	});
 
 
